fafcd6e3140b03cf95ea26a9112c8abdef252823
Sunee Thoedpraiphanawan <amythoedpranawan@yahoo.com>
2022-05-18 11:46:59 -0600

    commit tree 1f830882dbcc5094c8b4f737167a330cdfff7855

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for (let i = 0; i < namesArray.length; i++) {
+      thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+  }
+  return thankYouCards
+}
+
+function countDown( countdown ) {
+  while ( countdown > 0 ) {
+    console.log( countdown );
+    countdown -= 1;
+  }
+  console.log( countdown );
+}
+
+
+// function countDown( startingNumber ) {
+//   while ( startingNumber > 0 ) {
+//     console.log( startingNumber );
+//     startingNumber -= 1;
+//   }
+//   console.log( startingNumber );
+// }
+
